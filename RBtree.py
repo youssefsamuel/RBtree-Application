662c36580a1c691063dc331f2a772a322efa4b66
Youssef <youssefsamuel6167@gmail.com>
2022-05-02 22:53:01 +0200

    First Commit

---
+++ RBtree.py
@@ -0,0 +1,226 @@
+class Node:
+    def __init__(self, key = None):
+        self.key = key
+        self.left = None
+        self.right = None
+        self.parent = None
+        self.color = "RED"
+ 
+
+class RBtree:
+    def __init__(self):
+        self.tnil = Node()
+        self.tnil.color = "BLACK"
+        self.root = self.tnil
+        self.size = 0
+
+    def size(self):
+        return self.size
+
+    def max(a, b):
+        if a > b:
+            return a
+        return b    
+    def get_height(self, node):
+        if node == self.tnil:
+            return 0
+        return max(self.get_height(node.left), self.get_height(node.right)) + 1
+
+    def inorder(self, node):
+        if node != self.tnil:
+            self.inorder(node.left)
+            print(node.key)
+            self.inorder(node.right)
+
+    def search(self, node, key):
+        if node == self.tnil:
+            return False
+        if node.key.lower() == key.lower():
+            return True
+        if key.lower() < node.key.lower():
+            return self.search(node.left, key)
+        else:
+            return self.search(node.right, key)    
+
+    def rotate_left(self, node):
+        if node == self.tnil or node.right == self.tnil:
+            return 
+        y = node.right
+        if y.left:
+            node.right = y.left
+            y.left.parent = node
+        else:
+            node.right = self.tnil   
+        if node.parent:
+            y.parent = node.parent
+            if node.parent.left == node:
+                node.parent.left = y
+            else:
+                node.parent.right = y
+        else:
+            y.parent = self.tnil       
+        y.left = node
+        node.parent = y
+        return 
+
+    def rotate_right(self, node):
+        if node == self.tnil or node.left == self.tnil:
+            return    
+        y = node.left
+        if y.right:
+            node.left = y.right
+            y.right.parent = node
+        else:
+            node.left = self.tnil    
+        if node.parent:
+            y.parent = node.parent    
+            if node.parent.left == node:
+                node.parent.left = y
+            else:
+                node.parent.right = y
+        else:
+            y.parent = self.tnil       
+        y.right = node
+        node.parent = y
+        return 
+
+    def fix_insert(self, node):
+        if node.parent == self.tnil: 
+            node.color = "BLACK"
+            return
+        if node.parent.parent == self.tnil:
+            return   
+        while node.parent.color == "RED":
+            if node.parent.parent.left == node.parent:
+                if node.parent.parent.right == self.tnil:
+                    uncle_present = False
+                else:
+                    uncle_present = True
+                consider_Black = True    
+                if uncle_present:
+                    if node.parent.parent.right.color == "RED":
+                        consider_Black = False           
+                if uncle_present and not consider_Black:
+                    y = node.parent.parent.right
+                    node.parent.color = "BLACK"
+                    y.color = "BLACK"
+                    node.parent.parent.color = "RED"
+                    node = node.parent.parent
+                else:
+                        if node == node.parent.right:
+                            node = node.parent
+                            self.rotate_left(node)
+                        node.parent.color = "BLACK"
+                        node.parent.parent.color = "RED"
+                        self.rotate_right(node.parent.parent)
+                        node = node.parent
+            else:
+                if node.parent.parent.left == self.tnil:
+                    uncle_present = False
+                else:
+                    uncle_present = True
+                consider_Black = True    
+                if uncle_present:
+                    if node.parent.parent.left.color == "RED":
+                        consider_Black = False 
+                if uncle_present and not consider_Black:
+                    y = node.parent.parent.left
+                    if y.color == "RED": 
+                        node.parent.color = "BLACK"
+                        y.color = "BLACK"
+                        node.parent.parent.color = "RED"
+                        node = node.parent.parent
+                else:    
+                    if node == node.parent.left:
+                        node = node.parent
+                        self.rotate_right(node)
+                    node.parent.color = "BLACK"
+                    node.parent.parent.color = "RED"
+                    self.rotate_left(node.parent.parent)
+                    node = node.parent
+            if node.parent == self.tnil:
+                node.color = "BLACK"
+                self.root = node
+                return 
+            else:
+                if node.parent.color == "BLACK":
+                    return 
+             
+
+    def insert(self, node, key):
+        if node == self.tnil:
+            self.root = Node(key)
+            self.root.parent = self.tnil
+            self.root.left = self.tnil
+            self.root.right = self.tnil 
+            self.size = 1   
+            self.root.color = "BLACK" 
+            return
+        while True:
+            if node.key.lower() == key.lower():
+                return
+            if key.lower() < node.key.lower():
+                if node.left != self.tnil:
+                    node = node.left
+                else:
+                    node.left = Node(key)
+                    node.left.left = self.tnil
+                    node.left.right = self.tnil
+                    node.left.parent = node
+                    self.fix_insert(node.left)
+                    break
+            else:
+                if node.right != self.tnil:
+                    node = node.right
+                else:
+                    node.right = Node(key)
+                    node.right.parent = node
+                    node.right.left = self.tnil
+                    node.right.right = self.tnil
+                    self.fix_insert(node.right)
+                    break 
+        self.size += 1
+        return
+
+
+def load_dictionnary(filename, tree):
+    with open(filename) as f:
+        line = f.readline()
+        while line:
+            if line.endswith('\n'):
+                line = line[:-1]
+            tree.insert(tree.root, line)
+            line = f.readline()    
+    return tree
+
+def main():
+    t = RBtree()
+    filename = 'D:/CCE/6th_term/Data Structures/codes/EN-US-Dictionary.txt'
+    t = load_dictionnary(filename, t)
+    print("Dictioanny successfully loaded into Red-Black tree.")
+    while True:
+        print("Choose by typing the option number:")
+        print("1.Insert a new word.\n2.Search a word.\n3.Print the height of the tree.\n4.Print the size of the tree.")
+        n = input("Choice: ")
+        if n == "1":
+            str = input("Enter the new word: ")
+            if t.search(t.root, str) == True:
+                print("ERROR: Word already in the dictioanary!")
+            else:
+                t.insert(t.root, str)
+        elif n == "2": 
+            str = input("Enter the word you want to search: ")
+            if t.search(t.root, str) == True:
+                print("YES! The word is found.") 
+            else:
+                print("NO! The word is not found.")
+        elif n == "3":
+            print(f"Height = {t.get_height(t.root)}")              
+        elif n == "4":
+            print(f"Size = {t.size}")
+        else:
+            print("Invalid choice!")
+        more = input("If you want to perform another operation press 'y', else press any button: ")
+        if more != "y":
+            break 
+main()
